cad8829618407421cdf815a3fa5a26c10bef3f1e
J Xu <jxucoder@gmail.com>
2023-07-03 16:09:09 -0700

    add data

--- wfh_map_app/app.py
+++ wfh_map_app/app.py
@@ -7,6 +7,13 @@ from data_api import save_location, load_locations
 
 st.set_page_config(page_title="WFH Map Demo", page_icon=":world_map:️", layout="wide")
 st.title("WFH Map")
+
+github_logo_url = 'https://github.githubassets.com/images/modules/logos_page/Octocat.png'
+st.sidebar.image(github_logo_url, width=100)
+
+st.sidebar.title("a demo of [WFH Map](https://github.com/jxucoder/wfh_map/tree/master)")
+st.sidebar.write("It's not a working product. You should reconfigure the data_api before deploying internally. ")
+
 m = folium.Map(location=[40.092518, -94.98745], zoom_start=4.3)
 data = load_locations()
 
@@ -19,7 +26,7 @@ Draw(export=False, draw_options={
     'polyline': False, 'polygon': False, 'rectangle': False, 'circle': False, 'circlemarker': False, "marker": True
 }).add_to(m)
 
-output = st_folium(m, width=1024, height=500)
+output = st_folium(m, width=1256, height=600)
 if output["all_drawings"]:
     st.session_state['marker'] = output['all_drawings'][0]['geometry']['coordinates'][::-1]
 
